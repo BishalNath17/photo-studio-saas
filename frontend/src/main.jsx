b6283ed61bc2389c3844be06e6763b1458f72abc
BishalNath17 <bishal.westtilthai@gmail.com>
2026-03-13 19:25:46 +0530

    update website design and fixes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
-import { GoogleOAuthProvider } from '@react-oauth/google';
-
-const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || "YOUR_GOOGLE_CLIENT_ID_HERE";
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-      <App />
-    </GoogleOAuthProvider>
+    <App />
   </React.StrictMode>
 );
